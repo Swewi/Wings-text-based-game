04cbe3798ef4970891469673f4c0b3ed483f799e
Kiree Bellamy <kireebellamy@gmail.com>
2024-03-21 15:21:13 +0000

    Added ascii title and some basic structure

--- run.py
+++ run.py
@@ -1,1 +1,34 @@
+import sys
+import time
 
+from story import wings_text
+
+
+
+
+
+print("""
+'  WWWWWWWW                           WWWWWWWW iiii                                                        
+'  W::::::W                           W::::::Wi::::i                                                       
+'  W::::::W                           W::::::W iiii                                                        
+'  W::::::W                           W::::::W                                                             
+'   W:::::W           WWWWW           W:::::Wiiiiiiinnnn  nnnnnnnn       ggggggggg   ggggg    ssssssssss   
+'    W:::::W         W:::::W         W:::::W i:::::in:::nn::::::::nn    g:::::::::ggg::::g  ss::::::::::s  
+'     W:::::W       W:::::::W       W:::::W   i::::in::::::::::::::nn  g:::::::::::::::::gss:::::::::::::s 
+'      W:::::W     W:::::::::W     W:::::W    i::::inn:::::::::::::::ng::::::ggggg::::::ggs::::::ssss:::::s
+'       W:::::W   W:::::W:::::W   W:::::W     i::::i  n:::::nnnn:::::ng:::::g     g:::::g  s:::::s  ssssss 
+'        W:::::W W:::::W W:::::W W:::::W      i::::i  n::::n    n::::ng:::::g     g:::::g    s::::::s      
+'         W:::::W:::::W   W:::::W:::::W       i::::i  n::::n    n::::ng:::::g     g:::::g       s::::::s   
+'          W:::::::::W     W:::::::::W        i::::i  n::::n    n::::ng::::::g    g:::::g ssssss   s:::::s 
+'           W:::::::W       W:::::::W        i::::::i n::::n    n::::ng:::::::ggggg:::::g s:::::ssss::::::s
+'            W:::::W         W:::::W         i::::::i n::::n    n::::n g::::::::::::::::g s::::::::::::::s 
+'             W:::W           W:::W          i::::::i n::::n    n::::n  gg::::::::::::::g  s:::::::::::ss  
+'              WWW             WWW           iiiiiiii nnnnnn    nnnnnn    gggggggg::::::g   sssssssssss    
+'                                                                                 g:::::g                  
+'                                                                     gggggg      g:::::g                  
+'                                                                     g:::::gg   gg:::::g                  
+'                                                                      g::::::ggg:::::::g                  
+'                                                                       gg:::::::::::::g                   
+'                                                                         ggg::::::ggg                     
+'                                                                            gggggg                       
+""")
