86fbd49f034f8262db2bd4078a93a772bb5ee2f4
Kiree Bellamy <kireebellamy@gmail.com>
2024-03-22 09:54:17 +0000

    Added username entry and validation, and restart function

--- run.py
+++ run.py
@@ -52,10 +52,51 @@ def display_intro(self):
         """)
         print("""
          The goal of the game is to succesfully land your aircraft!
-                  You will be challenged at every step.
-                          So choose wisely
-
-                            Good luck!!
+                    Will you accept the challenge?
             """)
+
+    def get_username(self):
+        """
+        Prompt and validate user entered name.
+        """
+        while True:
+            try:
+                username = input("\nEnter your name: ").strip().lower()
+
+                # Checks if the username is empty or contains only spaces
+                if not username.strip():
+                    raise ValueError("Username cannot be empty or contain only spaces.")
+
+                # Checks if the username contains special characters
+                if not username.isalnum():
+                    raise ValueError("Username should contain only letters and numbers.")
+
+                # Checks if the username is too short or too long
+                if len(username) < 3 or len(username) > 20:
+                    raise ValueError("Username length should be between 3 and 20 characters.")  
+
+                return username
+
+            except ValueError as e:
+                print("Please try again.\n")
+
+    def restart_game(self):
+        """
+        Function to restart the game, regardless of outcome.
+        """
+        while True:
+            restart_choice = input("\nSo did you enjoy yourself? Do you want to play again?(yes/no): ").strip().lower()
+            if restart_choice.startswith('y'):
+                self.start_game()
+                break
+            elif restart_choice.startswith('n'):
+                self.type_text("""
+    **------------------------------------------------------------**
+                       Thanks for playing!!
+    **------------------------------------------------------------**
+                   """)
+                break
+            else:
+                print("\nUmm what?!?! I need a 'yes' or 'no'.")
 
     
